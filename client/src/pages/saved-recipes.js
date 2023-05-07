7084200d29a2f532150a23c6bb0f936756f81d66
CaptainSlow22 <razvandumitru2002@yahoo.com>
2023-05-07 18:22:05 +0300

    first commit

---
+++ client/src/pages/saved-recipes.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import axios from "axios";
+
+export const SavedRecipes = () => {
+  const [savedRecipes, setSavedRecipes] = useState([]);
+  const userID = useGetUserID();
+
+  useEffect(() => {
+    const fetchSavedRecipes = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/recipes/savedRecipes/${userID}`
+        );
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchSavedRecipes();
+  }, []);
+  return (
+    <div>
+      <h1>Saved Recipes</h1>
+      <ul>
+        {savedRecipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+              <img src={recipe.imageUrl} alt={recipe.name} />
+            </div>
+            <strong>Ingredients</strong>
+            {recipe.ingredients.map((r) => (
+                <p>{r}</p>
+            ))}
+            <strong>Instructions</strong>
+            <p>{recipe.instructions}</p>
+            <p>Cooking Time: {recipe.cookingTime} minutes</p>
+          </li>
+        )).reverse()}
+      </ul>
+    </div>
+  );
+};
